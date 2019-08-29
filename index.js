6ab446c67af0420ace5fda62839142aef8386f0c
Ammar <ammar_98@live.com>
2019-08-29 19:43:01 +0400

    Added get method that retrieves platform and gamertag.

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ dotenv.config({path: './config.env'})
 
 const port = process.env.PORT || 3000
 
+// Profile route
+app.use('/api/v1/profile', require('./routes/profile'))
+
 // Listens to the port and runs the server.
 app.listen(port, () => {
     console.log('Server is listening on port ' + port)
