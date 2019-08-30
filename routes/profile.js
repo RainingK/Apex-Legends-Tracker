06ca8010290ae03857593bd3f91a747fde7209f2
Ammar <ammar_98@live.com>
2019-08-30 11:17:18 +0400

    Displays an error message if the user is not found.

--- routes/profile.js
+++ routes/profile.js
@@ -13,6 +13,13 @@ router.get("/:platform/:gamertag", async function(req, res) {
 
         const data = await response.json()
 
+        // Check for errors
+        if (data.errors && data.errors.length > 0) {
+            return res.status(404).json({
+                message: 'Profile Not Found. Try with a different profile.'
+            })
+        }
+
         res.json(data);
     } catch (err) {
         console.log(err)
